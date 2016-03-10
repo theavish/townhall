4c6807d43d1f837a588339b0eb5039a894891272
Avi Samloff <avi.samloff@gmail.com>
2016-03-10 15:09:30 -0800

    adds comment list

--- client/commentList/commentListCtrl.js
+++ client/commentList/commentListCtrl.js
@@ -7,7 +7,21 @@
   commentListCtrl.$inject = [];
 
   function commentListCtrl() {
-    
+    var ctrl = this;
+
+    ctrl.comments = [{
+      id: 1,
+      text: 'this is a comment',
+      votes: 3,
+      author: 'Alex Jeng',
+      postDate: 1457650908742
+    }, {
+      id: 2,
+      text: 'this is another comment',
+      votes: 10,
+      author: 'Avi Samloff',
+      postDate: 1457651018742
+    }];
   }
-  
+
 })();
